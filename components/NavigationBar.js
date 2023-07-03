dbb5f7beb4aebb1757f6ef5818240e0416f930be
ahmet <adsayahmet94@hotmail.com>
2023-07-03 23:51:18 +0300

    added navbar

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,12 +1,34 @@
 import {AiFillHome} from "react-icons/ai";
 import { FiSave } from "react-icons/fi";
+import { AiOutlineSearch } from "react-icons/ai";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import Link from "next/link";
 
 const NavigationBar = () => {
 
 
   return (
-    <div>
+    <div className=" bg-slate-200 flex flex-col gap-4  w-[4rem] h-screen  items-center">
+        <div className=" flex flex-col gap-8 mt-8 h-1/2">
+        <Link href="search">
+        <AiFillHome size={50} />
+        </Link>
   
+        <Link href="search">
+        <AiOutlineSearch size={50} />
+        </Link>
+  
+        <Link href="search">
+        <AiFillPlayCircle size={50} />
+        </Link>
+        <Link href="search">
+        <FiSave size={50} />
+        </Link>
+        <Link href="search">
+        <CgProfile size={50} />
+        </Link>
+        </div>
     </div>
   );
 };
