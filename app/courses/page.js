268f441d131f9727425623451ea806526f619ebb
Alper Yazagan <alperyazagan@gmail.com>
2023-07-05 21:40:08 +0300

    navbar fix

--- app/courses/page.js
+++ app/courses/page.js
@@ -4,27 +4,34 @@ import CourseCard from "@/components/CourseCard";
 import CourseInfoCard from "@/components/CourseInfoCard";
 
 const page = () => {
+  const style = {
+    coursesDiv: `flex w-full md:ml-[4rem] flex-center`,
+    leftDiv: `w-full md:w-1/2 flex flex-col items-center bg-slate-100`,
+    rightDiv: `flex-col hidden md:w-1/2 md:flex justify-start items-center`,
+    heading: `p-4 text-lg text-slate-500 font-semibold`,
+    cardList: `flex flex-col justify-center items-center`,
+    btnDiv: `flex w-full justify-around mt-8`,
+    button: `bg-blue-500 rounded-full mx-2 p-2 text-xs text-white`,
+  };
+
   return (
-    <div className="flex w-full flex-center">
-      <div className="w-full md:w-1/2 flex flex-col items-center bg-slate-100">
-        <div className="p-4">
-          <h1 className="text-lg text-slate-500 font-semibold">My Courses</h1>
+    <div className={style.coursesDiv}>
+      <div className={style.leftDiv}>
+        <div className={style.heading}>
+          <h1>My Courses</h1>
         </div>
-        <div className="flex flex-col justify-center items-center">
+        <div className={style.cardList}>
+          <CourseCard />
           <CourseCard />
           <CourseCard />
           <CourseCard />
         </div>
       </div>
-      <div className="flex-col hidden md:w-1/2 md:flex justify-between items-center">
-        <CourseInfoCard className="w-full" />
-        <div className="flex w-full justify-around">
-          <button className="bg-blue-500 rounded-full mx-2 p-2 text-xs text-white">
-            REVIEW COURSE
-          </button>
-          <button className="bg-blue-500 rounded-full mx-2 p-2 text-xs text-white">
-            CONTINUE COURSE
-          </button>
+      <div className={style.rightDiv}>
+        <CourseInfoCard />
+        <div className={style.btnDiv}>
+          <button className={style.button}>REVIEW COURSE</button>
+          <button className={style.button}>CONTINUE COURSE</button>
         </div>
       </div>
     </div>
--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -6,27 +6,39 @@ import { CgProfile } from "react-icons/cg";
 import Link from "next/link";
 
 const NavigationBar = () => {
+  const style = {
+    navbar: `z-[2] md:bg-slate-100 backdrop-blur-md flex justify-center items-center sticky left-0 bottom-0 gap-6 w-full h-16 md:flex-col md:w-[4rem] md:h-screen`,
+    button: `flex flex-col items-center text-gray-400 hover:text-pink-400`,
+    animation: `animate-pulse`,
+  };
+
   return (
-    <div className=" bg-slate-200 flex sticky left-0 bottom-0 flex-col gap-4  w-[4rem] h-screen  items-center">
-      <div className=" flex flex-col gap-8 mt-8 h-1/2">
-        <Link href="home">
-          <AiFillHome size={50} />
-        </Link>
-        <Link href="search">
-          <AiOutlineSearch size={50} />
-        </Link>
-        <Link href="courses">
-          <AiFillPlayCircle size={50} />
-        </Link>
-        <Link href="saved">
-          <FiSave size={50} />
-        </Link>
-        <Link href="profile">
-          <CgProfile size={50} />
-        </Link>
-      </div>
+    <div className={style.navbar}>
+      <Link href="home" className={style.button}>
+        <AiFillHome size={30} />
+        <p>Home</p>
+      </Link>
+      <Link href="search" className={style.button}>
+        <AiOutlineSearch size={30} />
+        <p>Search</p>
+      </Link>
+      <Link href="courses" className={style.button}>
+        <AiFillPlayCircle size={30} />
+        <p>Courses</p>
+      </Link>
+      <Link href="saved" className={style.button}>
+        <FiSave size={30} />
+        <p>Saved</p>
+      </Link>
+      <Link href="profile" className={style.button}>
+        <CgProfile size={30} />
+        <p>Profile</p>
+      </Link>
     </div>
   );
 };
 
 export default NavigationBar;
+
+//<div className=" bg-slate-200 flex sticky left-0 bottom-0 flex-col gap-4  w-[4rem] h-screen  items-center">
+//  <div className=" flex flex-col justify-center gap-8 mt-8 h-1/2">
