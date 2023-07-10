079b09fe97bc97a0a41ea678d1bf774c77ea7e88
ahmet <adsayahmet94@hotmail.com>
2023-07-11 01:34:16 +0300

    added form

--- app/page.js
+++ app/page.js
@@ -5,28 +5,46 @@ import { BsFillKeyFill } from "react-icons/bs";
 import home from "@/public/assets/home.png";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import  Model  from "react-modal"
+import Form from "../components/Form"
 
-const page = () => {
+const Page = () => {
+
+  const [openForm, setOpenForm] = useState(false); 
+
+  const handleOpenForm = () => {
+    setOpenForm(!openForm);
+  };
+  
+
   return (
+
     <div className="flex ">
       <div className=" flex w-1/2 h-screen  bg-gray-200 items-center justify-center ">
         <div className="flex flex-col gap-5 items-center mx-16 ">
-          <BsFillKeyFill size={50} />
+          <BsFillKeyFill className=" bg-white p-2 w-16 rounded-2xl " size={50} />
           <h1>Discover passion</h1>
           <p>
             Find out what topics you find interesting, learn a new skill &
             connect with people that are passionate about similar topics.
           </p>
-          <button className=" text-white bg-cyan-400 rounded">
-            <Link href="home">Get Started</Link>
+          <button onClick={handleOpenForm} className=" text-white bg-blue-500 p-2 rounded-2xl">
+            {/* <Link href="home">Get Started</Link> */} onClick
           </button>
         </div>
       </div>
       <div className="flex w-1/2 h-screen items-center rounded-full ">
         <Image src={home} priority={true} alt="home-image" />
       </div>
+   
+       { openForm &&  <Form handleOpenForm={handleOpenForm}/>}
+     
+
+
+
     </div>
+
   );
 };
 
-export default page;
+export default Page;
