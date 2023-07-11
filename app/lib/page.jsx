c57f4b6c0d7ef9ba4eb67e009a01c141eb0b802e
ahmet <adsayahmet94@hotmail.com>
2023-07-12 00:07:29 +0300

    added firebase

---
+++ app/lib/page.jsx
@@ -0,0 +1,23 @@
+'use client'
+import { useState } from "react";
+import {  useEffect } from "react";
+import { db } from "./firebase";
+import { query, collection, onSnapshot } from "firebase/firestore";
+
+export default function Page() {
+
+const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "courses"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const courses = [];
+      querySnapshot.forEach((doc) => {
+        courses.push({...doc.data(), id: doc.id});
+      });
+      setItems(courses);
+    });
+    console.log(items);
+  }, []);
+  return <div>Page</div>;
+}
