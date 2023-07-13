9c7a3f54f440fb1179d589f86c8474a7f8e66bfc
Dalia Khalifa <daliakhalifa02@gmail.com>
2023-07-14 02:13:49 +0300

    Part 1

--- app/saved/page.js
+++ app/saved/page.js
@@ -5,7 +5,7 @@ import SavedCoursesInfo from "@/components/SavedCoursesInfo";
 
 const style = {
   container: `flex flex-col md:flex-row gap-10 w-full p-4`,
-  card: `w-1/2`,
+  card: `w-1/2 flex-col hidden md:w-1/2 md:flex justify-start items-center`,
   title: `text-center font-bold text-2xl`,
 };
 
@@ -14,7 +14,7 @@ const page = () => {
     <div>
       <h1 className={style.title}>Saved Courses</h1>
       <div className={style.container}>
-        <div>
+        <div className={style.leftDiv}>
           <SavedCourses />
           <SavedCourses />
           <SavedCourses />
