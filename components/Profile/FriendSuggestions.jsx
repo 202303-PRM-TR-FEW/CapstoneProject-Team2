24df77572691e8354b8768fa1f483b1b5d6b5077
ahmet <adsayahmet94@hotmail.com>
2023-07-15 04:29:20 +0300

    rendered the users from firestore and arrenge the buttons

--- components/Profile/FriendSuggestions.jsx
+++ components/Profile/FriendSuggestions.jsx
@@ -1,53 +1,95 @@
+"use client";
 import Image from "next/image";
 import profile from "../../public/assets/alper-yazagan.jpg";
+import { fetchUsers } from "../../redux/apiUsers";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const FriendSuggestions = () => {
-    return (
-      <div className="">
-        <h1 className="font-bold mt-8">Friend Suggestions</h1>
-        <div className="mt-8 bg-white w-[26rem] p-4 h-40 rounded-xl">
-          <div className="flex justify-around">
-            <div className="flex gap-2">
-              <Image
-                className="w-8"
-                src={profile}
-                alt="profile"
-                priority={true}
-              />
-              <h1>Laura Evans</h1>
-            </div>
-            <div className="flex gap-4">
-              <button className="bg-indigo-400 rounded-xl p-1 text-white">
-                Follow
-              </button>
-              <button className="bg-indigo-400 rounded-xl p-1 text-white">
-                Message
-              </button>
-            </div>
-          </div>
-          <div className="flex mt-6 justify-around">
-            <div className="flex gap-2">
-              <Image
-                className="w-8"
-                src={profile}
-                alt="profile"
-                priority={true}
-              />
-              <h1>Laura Evans</h1>
-            </div>
-            <div className="flex gap-4">
-              <button className="bg-indigo-400 rounded-xl p-1 text-white">
-                Follow
-              </button>
-              <button className="bg-indigo-400 rounded-xl p-1 text-white">
-                Message
-              </button>
-            </div>
-          </div>
-          <hr className="mt-4" />
-        </div>
-      </div>
-    );
-  };
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users.users);
+  const [showAll, setShowAll] = useState(false);
 
-export default FriendSuggestions
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  console.log(users);
+  return (
+    <div className="">
+      <h1 className="font-bold mt-8">Friend Suggestions</h1>
+      <div className=" flex flex-col gap-2 mt-8 bg-white w-[26rem] p-4  rounded-xl">
+        {showAll
+          ? users.slice(0,4).map((user) => (
+              <div key={user.id} className="flex justify-around gap-2">
+                <div className="flex gap-2">
+                  <Image
+                    className="w-8"
+                    src={profile}
+                    alt="profile"
+                    priority={true}
+                  />
+                  <div className=" w-16">
+                    <h1>{user.first}</h1>
+                    <h1>{user.last}</h1>
+                  </div>
+                </div>
+                <div className="flex gap-4">
+                  <button className="bg-indigo-400 rounded-xl p-1 text-white">
+                    Follow
+                  </button>
+                  <button className="bg-indigo-400 rounded-xl p-1 text-white">
+                    Message
+                  </button>
+                </div>
+              </div>
+            ))
+          : users.slice(0, 2).map((user) => (
+              <div key={user.id} className="flex justify-around gap-2">
+                <div className="flex gap-2">
+                  <Image
+                    className="w-8"
+                    src={profile}
+                    alt="profile"
+                    priority={true}
+                  />
+                  <div className=" w-16">
+                    <h1>{user.first}</h1>
+                    <h1>{user.last}</h1>
+                  </div>
+                </div>
+                <div className="flex gap-4">
+                  <button className="bg-indigo-400 w-16 rounded-xl p-1 text-white">
+                    Add
+                  </button>
+                  <button className="bg-indigo-400 rounded-xl p-1 text-white">
+                    Message
+                  </button>
+                </div>
+              </div>
+            ))}
+
+        <hr className="mt-4" />
+        {showAll ? (
+            <button 
+                className="bg-red-400 text-white w-16 " 
+                onClick={() => setShowAll(!showAll)}
+            >
+                View Less
+            </button>
+        ) :
+
+        <button
+          className=" bg-red-400 text-white w-16"
+          onClick={() => setShowAll(!showAll)}
+        >
+          View All
+        </button>
+}
+      </div>
+    </div>
+  );
+};
+
+export default FriendSuggestions;
--- components/Profile/FriendsList.jsx
+++ components/Profile/FriendsList.jsx
@@ -1,55 +1,51 @@
+"use client";
+
 import Button from "./Button";
 import Image from "next/image";
 import profile from "../../public/assets/alper-yazagan.jpg";
+import { fetchUsers } from "../../redux/apiUsers";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 const FriendsList = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  console.log(users);
+
   return (
-    <div>
-      <h1 className="mt-8 font-bold">Friends</h1>
-      <div className="mt-8 bg-white w-[26rem] p-4 h-40 rounded-xl">
-        <div className="flex justify-around">
-          <div className="flex gap-2">
-            <Image
-              className="w-8"
-              src={profile}
-              alt="profile"
-              priority={true}
-            />
-            <h1>Laura Evans</h1>
+    <div className=" mt-12">
+      <h1 className="font-bold mt-8">Friend </h1>
+      <div className="flex flex-col gap-4 bg-white w-[26rem] h-[20] p-4  rounded-xl mt-8">
+        {users.slice(0, 2).map((user) => (
+          <div key={user.id} className="flex justify-around gap-2">
+            <div className="flex gap-2">
+              <Image
+                className="w-8"
+                src={profile}
+                alt="profile"
+                priority={true}
+              />
+              <div className=" w-16">
+                <h1>{user.first}</h1>
+                <h1>{user.last}</h1>
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <button className="bg-indigo-400 rounded-xl p-1 text-white">
+                Delete
+              </button>
+
+              <button className="bg-indigo-400 rounded-xl p-1 text-white">
+                Message
+              </button>
+            </div>
           </div>
-          <div className="flex gap-4">
-            <button className="bg-indigo-400 rounded-xl p-1 text-white">
-              Profile
-            </button>
-            <button className="bg-indigo-400 rounded-xl p-1 text-white">
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className="flex mt-6 justify-around">
-          <div className="flex gap-2">
-            <Image
-              className="w-8"
-              src={profile}
-              alt="profile"
-              priority={true}
-            />
-            <h1>Laura Evans</h1>
-          </div>
-          <div className="flex gap-4">
-            <button className="bg-indigo-400 rounded-xl p-1 text-white">
-              Profile
-            </button>
-            <button className="bg-indigo-400 rounded-xl p-1 text-white">
-              Delete
-            </button>
-          </div>
-        </div>
-        <hr className="mt-4" />
-      </div>
-      <div className="flex gap-4 mt-8">
-        <Button text="FIND FRIENDS" />
-        <Button text="INVITE FRIENDS" />
+        ))}
       </div>
     </div>
   );
