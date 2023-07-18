dfd210b3c332477c88dbb2795df64fd69e1485eb
Alper Yazagan <alperyazagan@gmail.com>
2023-07-18 20:00:53 +0300

    profile page  fix

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,21 +1,20 @@
-import ProfileCard from "../../components/profile/ProfileCard";
-import StatisticsCard from "../../components/profile/StatisticsCard";
-import AchievementCard from "../../components/profile/AchievementCard";
-import FriendSuggestions from "../../components/profile/FriendSuggestions";
-import FriendsList from "../../components/profile/FriendsList";
-
+import ProfileCard from "../../components/Profile/ProfileCard";
+import StatisticsCard from "../../components/Profile/StatisticsCard";
+import AchievementCard from "../../components/Profile/AchievementCard";
+import FriendSuggestions from "../../components/Profile/FriendSuggestions";
+import FriendsList from "../../components/Profile/FriendsList";
 
 const page = () => {
   return (
     <div className="md:flex md:flex-row  md:items-center md:justify-center mx-[5rem] flex flex-col items-center gap-12">
       <div>
-      <ProfileCard />
-      <StatisticsCard />
-      <AchievementCard />
+        <ProfileCard />
+        <StatisticsCard />
+        <AchievementCard />
       </div>
       <div>
-      <FriendSuggestions  />
-      <FriendsList />
+        <FriendSuggestions />
+        <FriendsList />
       </div>
     </div>
   );
