f12cbb8cd1636ce8efeeb278578b575b4938a68c
ahmet <adsayahmet94@hotmail.com>
2023-07-19 11:32:25 +0300

    navigates specific user page

--- components/Profile/FriendsList.jsx
+++ components/Profile/FriendsList.jsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 
 import { fetchUsers } from "../../redux/apiUsers";
 import { useSelector, useDispatch } from "react-redux";
-import {  removeSelectedProfile } from "../../redux/apiUsers";
+import { removeSelectedProfile } from "../../redux/apiUsers";
 import { useEffect } from "react";
+import Link from "next/link";
 
 const FriendsList = () => {
   const dispatch = useDispatch();
@@ -20,43 +21,38 @@ const FriendsList = () => {
     dispatch(removeSelectedProfile(selectedProfile));
   };
 
-
-
-
   return (
     <div className=" mt-12">
       <h1 className="font-bold mt-8">Friends</h1>
       <div className="flex flex-col gap-4 bg-white w-[26rem] h-[20] p-4  rounded-xl mt-8">
-      
-
         {selectedProfiles.map((selectedProfile) => (
-         <div key={selectedProfile.id} className=" items-center flex justify-around gap-2">
-         <div className="flex gap-2 ">
-           <img
-             className="w-8 rounded-full  w-[90px] h-[90px]  "
-             src={selectedProfile.image}
-             alt="profile"
-           
-           
-           />
-          
-         </div>
-         <div className=" w-16">
-             <h1>{selectedProfile.first}</h1>
-             <h1>{selectedProfile.last}</h1>
-           </div>
-         <div className="flex gap-4">
-           <button
-             onClick={() => handleDeleteButtonClick(selectedProfile)}
-             className="bg-indigo-400 h-16 rounded-xl p-1 text-white"
-           >
-             Delete
-           </button>
-           <button className="bg-indigo-400 h-16 rounded-xl p-1 text-white">
-             View
-           </button>
-         </div>
-       </div>
+          <div
+            key={selectedProfile.id}
+            className=" items-center flex justify-around gap-2"
+          >
+            <div className="flex gap-2 ">
+              <img
+                className="w-8 rounded-full  w-[90px] h-[90px]  "
+                src={selectedProfile.image}
+                alt="profile"
+              />
+            </div>
+            <div className=" w-16">
+              <h1>{selectedProfile.first}</h1>
+              <h1>{selectedProfile.last}</h1>
+            </div>
+            <div className="flex gap-4">
+              <button
+                onClick={() => handleDeleteButtonClick(selectedProfile)}
+                className="bg-indigo-400 h-16 rounded-xl p-1 text-white"
+              >
+                Delete
+              </button>
+              <button className="bg-indigo-400 h-16 rounded-xl p-1 text-white">
+                <Link href={`/profile/${selectedProfile.id}`}>View</Link>
+              </button>
+            </div>
+          </div>
         ))}
       </div>
     </div>
