1a3274bfbbb52b1d1aca8fb507547f7856eab37f
Alper Yazagan <alperyazagan@gmail.com>
2023-07-21 12:54:08 +0300

    muhanneds fix method applied

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -26,60 +26,86 @@ const NavigationBar = () => {
     animation: `animate-pulse`,
     burgerBtnDiv: `flex flex-col h-12 w-12 justify-center items-center group`,
     hamburgerLine: `h-1 w-6 my-1 rounded-full bg-gray-400 transition ease transform duration-300`,
+    navbar2: `flex`,
+    extrabox2: `flex`,
+    extrabox: `z-[2] md:bg-slate-100 backdrop-blur-md flex flex-col justify-center items-center sticky left-0 bottom-16 gap-2 w-full h-32 md:flex-col md:w-[4rem] md:h-screen dark:bg-slate-700`,
   };
 
   return (
-    <div className={style.navbar}>
-      <Link href="home" className={style.button}>
-        <AiFillHome size={30} />
-        <p>{t("Home")}</p>
-      </Link>
-      <Link href="search" className={style.button}>
-        <AiOutlineSearch size={30} />
-        <p>{t("Search")}</p>
-      </Link>
-      <Link href="courses" className={style.button}>
-        <AiFillPlayCircle size={30} />
-        <p>{t("Courses")}</p>
-      </Link>
-      <Link href="saved" className={style.button}>
-        <FiSave size={30} />
-        <p>{t("Saved")}</p>
-      </Link>
-      <Link href="profile" className={style.button}>
-        <CgProfile size={30} />
-        <p>{t("Profile")}</p>
-      </Link>
-      <button className={style.burgerBtnDiv} onClick={() => setIsOpen(!isOpen)}>
-        <div
-          className={`${style.hamburgerLine} ${
-            isOpen
-              ? "rotate-45 translate-y-3  group-hover:bg-[#F472B6]"
-              : "group-hover:bg-[#F472B6]"
-          }`}
-        />
-        <div
-          className={`${style.hamburgerLine} ${
-            isOpen ? "opacity-0" : "group-hover:bg-[#F472B6]"
-          }`}
-        />
-        <div
-          className={`${style.hamburgerLine} ${
-            isOpen
-              ? "-rotate-45 -translate-y-3 group-hover:bg-[#F472B6]"
-              : "group-hover:bg-[#F472B6]"
-          }`}
-        />
-      </button>
-      {/*
-      <Link href="/" locale="kr" className={style.button}>
-        <CgProfile size={30} />
-        <p>Korean</p>
-      </Link>
-      <Providers>
-        <ThemeSwitcher />
-      </Providers>
-       */}
+    <div className="z-[2] md:bg-slate-100 backdrop-blur-md flex flex-col-reverse justify-center items-center sticky left-0 bottom-0 gap-4 w-full h-16 md:flex-col md:w-[4rem] md:h-screen dark:bg-slate-700 ">
+      <div className={style.navbar2}>
+        <Link href="home" className={style.button}>
+          <AiFillHome size={30} />
+          <p>{t("Home")}</p>
+        </Link>
+        <Link href="search" className={style.button}>
+          <AiOutlineSearch size={30} />
+          <p>{t("Search")}</p>
+        </Link>
+        <Link href="courses" className={style.button}>
+          <AiFillPlayCircle size={30} />
+          <p>{t("Courses")}</p>
+        </Link>
+        <Link href="saved" className={style.button}>
+          <FiSave size={30} />
+          <p>{t("Saved")}</p>
+        </Link>
+        <Link href="profile" className={style.button}>
+          <CgProfile size={30} />
+          <p>{t("Profile")}</p>
+        </Link>
+        <button
+          className={style.burgerBtnDiv}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div
+            className={`${style.hamburgerLine} ${
+              isOpen
+                ? "rotate-45 translate-y-3  group-hover:bg-[#F472B6]"
+                : "group-hover:bg-[#F472B6]"
+            }`}
+          />
+          <div
+            className={`${style.hamburgerLine} ${
+              isOpen ? "opacity-0" : "group-hover:bg-[#F472B6]"
+            }`}
+          />
+          <div
+            className={`${style.hamburgerLine} ${
+              isOpen
+                ? "-rotate-45 -translate-y-3 group-hover:bg-[#F472B6]"
+                : "group-hover:bg-[#F472B6]"
+            }`}
+          />
+        </button>
+      </div>
+      {isOpen && (
+        <div className={style.extrabox2}>
+          <Link href="/" locale="en" className={style.button}>
+            <CgProfile size={30} />
+            <p>English</p>
+          </Link>
+          <Link href="/" locale="de" className={style.button}>
+            <CgProfile size={30} />
+            <p>German</p>
+          </Link>
+          <Link href="/" locale="kr" className={style.button}>
+            <CgProfile size={30} />
+            <p>Korean</p>
+          </Link>
+          <Link href="/" locale="tr" className={style.button}>
+            <CgProfile size={30} />
+            <p>Turkish</p>
+          </Link>
+          <Link href="/" locale="ar" className={style.button}>
+            <CgProfile size={30} />
+            <p>Arabic</p>
+          </Link>
+          <Providers>
+            <ThemeSwitcher />
+          </Providers>
+        </div>
+      )}
     </div>
   );
 };
--- darkMode/ThemeSwitcher.js
+++ darkMode/ThemeSwitcher.js
@@ -1,45 +1,39 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-import {BsFillMoonFill} from 'react-icons/bs';
-import {BsFillSunFill} from 'react-icons/bs';
+import { BsFillMoonFill } from "react-icons/bs";
+import { BsFillSunFill } from "react-icons/bs";
 
 const ThemeSwitcher = () => {
-    const [mounted, setMounted] = useState(false);
-    const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-    const currentTheme = theme === 'system' ? 'light' : theme;
+  const currentTheme = theme === "system" ? "light" : theme;
 
-    useEffect(() => {
-        setMounted(true);
-    }, []);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-    if (!mounted){
-        return null;
-    }
+  if (!mounted) {
+    return null;
+  }
 
-    return (
-        <div>
-            <div>
-                {currentTheme === 'dark' ? (
-                    <button 
-                        className="p-4"
-                        onClick={() => setTheme('light')}
-                    >
-                        {' '}
-                        <BsFillSunFill size={25} />
-                    </button>
-                ) : (
-                    <button
-                        className="p-4"
-                        onClick={() => setTheme('dark')}
-                    >
-                        <BsFillMoonFill size={25}/>
-                    </button>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        {currentTheme === "dark" ? (
+          <button className="p-2" onClick={() => setTheme("light")}>
+            {" "}
+            <BsFillSunFill size={25} />
+          </button>
+        ) : (
+          <button className="p-2" onClick={() => setTheme("dark")}>
+            <BsFillMoonFill size={25} />
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ThemeSwitcher;
