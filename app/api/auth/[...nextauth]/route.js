bd4a311e07b8748343240d5caa2f787eddafbc06
Alper Yazagan <alperyazagan@gmail.com>
2023-07-30 16:27:10 +0300

    firebase adapter setup

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
 
 const handler = NextAuth({
   providers: [
@@ -13,6 +15,13 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  adapter: FirestoreAdapter({
+    credentials: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  }),
 });
 
 export { handler as GET, handler as POST };
